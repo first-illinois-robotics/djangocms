d01c189db4b040cbafe41188ef74d1d1bd081d76
devyntk <bkeeneykid@me.com>
2021-09-16 18:37:01 -0500

    Prep for deploy

--- firweb/settings/__init__.py
+++ firweb/settings/__init__.py
@@ -1,13 +1,6 @@
 from .common import *
 
-try:
-    FIR_ENV = os.environ["FIR_ENV"]
-except KeyError:
-    print("No valid 'FIR_ENV' environment variable found, assuming 'dev'")
-    FIR_ENV = "dev"
-
-
-if FIR_ENV == "prod":
+if os.environ.get("GOOGLE_CLOUD_PROJECT", None):
     # both prod and staging use the same config, the secrets will just import from different projects
     from .prod import *
 else:
--- firweb/settings/prod.py
+++ firweb/settings/prod.py
@@ -3,17 +3,23 @@ import io
 
 import environ
 from google.cloud import secretmanager
+from .common import BASE_DIR
 
 env = environ.Env()
+env_file = os.path.join(BASE_DIR, ".env")
 
 project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
 
-client = secretmanager.SecretManagerServiceClient()
-settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
-name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
-payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
+if os.path.isfile(env_file):
+    # Use a local secret file, if provided
+    env.read_env(env_file)
+else:
+    client = secretmanager.SecretManagerServiceClient()
+    settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
+    name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
+    payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
 
-env.read_env(io.StringIO(payload))
+    env.read_env(io.StringIO(payload))
 
 DEBUG = False
 
@@ -23,5 +29,9 @@ DATABASES = {
     'default': env.db(),
 }
 
+DATABASES['default']['NAME'] = 'firweb_staging'
+
+print(DATABASES)
+
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
-GS_BUCKET_NAME = env.str("gs_bucket")
+GS_BUCKET_NAME = env.str("GS_BUCKET_NAME")
