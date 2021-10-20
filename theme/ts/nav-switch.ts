e09dd0e26da0b93bd65ed69e19fa1dd1cec9eedb
devyntk <bkeeneykid@me.com>
2021-10-19 22:50:29 -0500

    Make ESLint Happy

--- theme/ts/nav-switch.ts
+++ theme/ts/nav-switch.ts
@@ -1,11 +1,11 @@
-let programs: string[] = ['flle', 'fllc', 'ftc', 'frc'];
+const programs: string[] = ['flle', 'fllc', 'ftc', 'frc'];
 
-let section:HTMLElement = document.getElementById("main-program-navigation");
+const section:HTMLElement = document.getElementById("main-program-navigation");
 const program_logos = section.getElementsByClassName("program-nav-box");
 for (let i = 0; i < program_logos.length; i++) {
     const logo = program_logos[i] as HTMLElement;
     logo.addEventListener("mouseenter", function(e) {
-        for (let prog of programs) {
+        for (const prog of programs) {
             const menu = document.getElementById(prog + "-menu");
             if ((<Element>e.target).id.startsWith(prog)) {
                 menu.style.display = "block";
