9981d347534a49f6d89d9dd0dc625a4b6f40fd89
devyn <bkeeneykid@me.com>
2021-12-18 17:21:14 -0600

    Fix header

--- theme/ts/nav-switch.ts
+++ theme/ts/nav-switch.ts
@@ -1,9 +1,14 @@
-const programs: string[] = ['flle', 'fllc', 'ftc', 'frc'];
+const programs: string[] = ['main', 'flle', 'fllc', 'ftc', 'frc'];
 
 const section:HTMLElement = document.getElementById("main-program-navigation");
 const program_logos = section.getElementsByClassName("program-nav-box");
+
 for (let i = 0; i < program_logos.length; i++) {
     const logo = program_logos[i] as HTMLElement;
+    if (logo.classList.contains("flld-brand")) {
+        continue // no sub-menu for FLL Discover, so don't update anything if moused over
+    }
+
     logo.addEventListener("mouseenter", function(e) {
         for (const prog of programs) {
             const menu = document.getElementById(prog + "-menu");
