28c5064205bfac4273f1291fd6126a1c9ecbc318
devyn <bkeeneykid@me.com>
2022-02-24 10:56:38 -0600

    disable caching

--- firweb/settings/prod.py
+++ firweb/settings/prod.py
@@ -68,11 +68,11 @@ ALLOWED_HOSTS = ["*"]
 GOOGLE_MEASUREMENT_ID = env.str("GOOGLE_MEASUREMENT_ID", None)
 
 if not os.getenv("USE_CLOUD_SQL_AUTH_PROXY", None):
-    CACHES = {
-        "default": {
-            "BACKEND": "firweb.backends.GaeMemcachedCache",
-        }
-    }
+    # CACHES = {
+    #     "default": {
+    #         "BACKEND": "firweb.backends.GaeMemcachedCache",
+    #     }
+    # }
 
     EMAIL_BACKEND = "firweb.backends.GaeMailBackend"
     DEFAULT_FROM_EMAIL = "noreply@firstillinoisrobotics.org"
